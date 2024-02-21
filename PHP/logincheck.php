eeaa2759468fc1135a44d866bda14b9fc3ffcf34
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-02-21 20:21:06 +0530

    update

--- PHP/logincheck.php
+++ PHP/logincheck.php
@@ -1,13 +1,12 @@
 <?php
-	session_start();
-	if (!isset($_SESSION['id'])) {
-		
+    session_start();
+    if (!isset($_SESSION['id']) || empty($_SESSION['id'])) {
+        // If the 'id' session variable is not set or empty, redirect the user to the home page with an error message
         echo '<script type="text/javascript">'; 
-echo 'alert("Invalid Session");'; 
-echo 'window.location.href = "home.php";';
-echo '</script>'; 
-
-	}
-
-
+        echo 'alert("Invalid Session");'; 
+        echo 'window.location.href = "home.php";';
+        echo '</script>'; 
+        exit; // Stop further execution of the script
+    }
 ?>
+
