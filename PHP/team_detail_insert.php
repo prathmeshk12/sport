f0b7b79b2dd39eed740a4ceb86d96df98dccacf8
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-02-24 23:11:43 +0530

    update

--- PHP/team_detail_insert.php
+++ PHP/team_detail_insert.php
@@ -45,7 +45,8 @@ if (isset($_POST['btn_t'])) {
         }
     } else {
         // Session variable not set
-        echo "<script>alert('Submitted Sucessfully');</script>";
+        echo "<script>alert('Submitted Sucessfully');window.location='index1.php'</script>";
+        exit;
         header("Location: index1.php");
     }
 } else {
