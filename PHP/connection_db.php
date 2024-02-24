22391b3efd499ce5583125c53306e56e1c099b77
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-02-24 17:47:49 +0530

    update

--- PHP/connection_db.php
+++ PHP/connection_db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "apache.mysql.database.azure.com";
-$username = $_ENV['MYSQL_USERNAME'];
-$password = $_ENV['MYSQL_PASSWORD'];
+$username = $_ENV['AZURE_MYSQL_USERNAME'];
+$password = $_ENV['AZURE_MYSQL_PASSWORD'];
 $database="sportclub";
 $con = new mysqli($servername, $username, $password, $database);
 
