4ca9e9962c1045a557fc2de958679b001fc3da7b
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-02-24 12:46:29 +0530

    update

--- about.php
+++ about.php
@@ -63,7 +63,7 @@
 		?>
 	</nav>
 
-	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(image1/images/img_bg_2.jpg);" data-stellar-background-ratio="0.5">
+	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(images1/images/img_bg_2.jpg);" data-stellar-background-ratio="0.5">
 		<div class="overlay"></div>
 		<div class="container">
 			<div class="row">
