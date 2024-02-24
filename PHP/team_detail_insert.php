2b7fb850543d3e57358e086506157badffcfc11e
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-02-24 22:47:44 +0530

    update

--- PHP/team_detail_insert.php
+++ PHP/team_detail_insert.php
@@ -1,38 +1,47 @@
 <?php
- session_start();
-  $id=$_SESSION['id'];
-   if(isset($_POST['btn_t'])){
+session_start();
 
-   $tname=$_POST['tname'];
-   $cname=$_POST['tcaptian'];
-   $total=$_POST['totalp'];
-   $participate=$_POST['participate'];
-   $extra=$_POST['extra'];
-   
-  
-  // include('./connection_db.php');
-  $con = new mysqli(
-		"apache.mysql.database.azure.com",
-		$_ENV['AZURE_MYSQL_USERNAME'],
-		$_ENV['AZURE_MYSQL_PASSWORD'],
-		"sportclub"
-	);
-$q = "INSERT INTO team_detail (user_id, team_name, team_captain, total_player, participate_player, extra_player) VALUES ('$id','$tname','$cname','$total','$participate','$extra')";
-  $data=mysqli_query($con,$q);
-    if($data){
-     
-       
-#echo 'alert("Registration Successfully");'; 
-header("Location: ../player_registration.php");
-        exit;
+// Check if the form is submitted
+if (isset($_POST['btn_t'])) {
+    // Retrieve form data
+    $tname = $_POST['tname'];
+    $cname = $_POST['tcaptian'];
+    $total = $_POST['totalp'];
+    $participate = $_POST['participate'];
+    $extra = $_POST['extra'];
+    
+    // Check if session variable exists
+    if (isset($_SESSION['id'])) {
+        $id = $_SESSION['id'];
+
+        // Include database connection
+        include('./connection_db.php');
+
+        // Prepare and execute the SQL query
+        $q = "INSERT INTO team_detail (user_id, team_name, team_captain, total_player, participate_player, extra_player) VALUES ('$id','$tname','$cname','$total','$participate','$extra')";
+        $con = new mysqli(
+          "apache.mysql.database.azure.com",
+          $_ENV['AZURE_MYSQL_USERNAME'],
+          $_ENV['AZURE_MYSQL_PASSWORD'],
+          "sportclub"
+      );
+        $data = mysqli_query($con, $q);
+
+        // Check if the query was successful
+        if ($data) {
+            // Redirect after successful insertion
+            header("Location: ../player_registration.php");
+            exit;
+        } else {
+            // Display error message
+            echo "Failed to insert data: " . mysqli_error($con);
+        }
+    } else {
+        // Session variable not set
+        echo "Session variable 'id' not set.";
     }
-  else{
-    echo"faild";
-  }
-   }
-
-
-
-
-
+} else {
+    // Form not submitted
+    echo "Form not submitted.";
+}
 ?>
