8c562e654e97e5cfd64097d97fa835eb9ee2af56
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-02-25 11:03:13 +0530

    update

--- view_players.php
+++ view_players.php
@@ -111,13 +111,14 @@
 			
 			<div class="row row-bottom-padded-md">
  <?php
-           $id=$_SESSION['id'];
+           
 		   $servername = "apache.mysql.database.azure.com";
 		   $username = $_ENV['MYSQL_USERNAME'];
 		   $password = $_ENV['MYSQL_PASSWORD'];
 		   $database="sportclub";
             include('./PHP/connection_db.php');
 			$con = new mysqli($servername, $username, $password, $database);
+			$id=$_SESSION['id'];
               $sql="SELECT p_id, player_name, p_designation, p_type, contact, team_name, p_image FROM player_detail WHERE user_id='$id'";
                $query=mysqli_query($con,$sql);
               while($row=mysqli_fetch_array($query)){
