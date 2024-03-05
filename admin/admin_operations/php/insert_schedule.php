66f5b137a0e9e6b0fbd589ccaa01bb9e89be5a5e
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-03-05 21:27:38 +0530

    update

--- admin/admin_operations/php/insert_schedule.php
+++ admin/admin_operations/php/insert_schedule.php
@@ -11,7 +11,12 @@ $mtime=$_POST['m_time'];
 
 
 include('connection_db.php');
-
+$con = new mysqli(
+  "apache.mysql.database.azure.com",
+  $_ENV['AZURE_MYSQL_USERNAME'],
+  $_ENV['AZURE_MYSQL_PASSWORD'],
+  "sportclub"
+);
 $sql="INSERT INTO schedule(game_name,groupA_team_name,groupB_team_name,place,  s_date,s_time)VALUES('$gname','$teama','$teamb','$place','$mdate','$mtime')";
   $data=mysqli_query($con,$sql);
   if($data){
