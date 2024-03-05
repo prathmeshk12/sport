87d531b56f3335632b7fd0823ecdaa46e389ef00
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-03-05 21:35:55 +0530

    update

--- admin/admin_operations/php/insert_schedule.php
+++ admin/admin_operations/php/insert_schedule.php
@@ -1,40 +1,29 @@
 <?php
- session_start();
-if(isset($_POST['schedule'])){
+session_start();
 
-$gname=$_POST['g_name'];
-$teama=$_POST['team_a'];
-$teamb=$_POST['team_b'];
-$place=$_POST['place'];
-$mdate=$_POST['m_date'];
-$mtime=$_POST['m_time'];
+if (isset($_POST['schedule'])) {
+    $gname = $_POST['g_name'];
+    $teama = $_POST['team_a'];
+    $teamb = $_POST['team_b'];
+    $place = $_POST['place'];
+    $mdate = $_POST['m_date'];
+    $mtime = $_POST['m_time'];
 
+    include('connection_db.php');
 
-include('connection_db.php');
-$con = new mysqli(
-  "apache.mysql.database.azure.com",
-  $_ENV['AZURE_MYSQL_USERNAME'],
-  $_ENV['AZURE_MYSQL_PASSWORD'],
-  "sportclub"
-);
-$sql="INSERT INTO schedule(game_name,groupA_team_name,groupB_team_name,place,  s_date,s_time)VALUES('$gname','$teama','$teamb','$place','$mdate','$mtime')";
-  $data=mysqli_query($con,$sql);
-  if($data){
-  	echo '<script type="text/javascript">'; 
-echo 'alert("Schedule added successfully");'; 
-echo 'window.location.href = "../add_scheduel.php";';
-echo '</script>'; 
-  }
-  else{
-  	echo '<script type="text/javascript">'; 
-echo 'alert("Error.. Please check detail");'; 
-echo 'window.location.href = "../add_scheduel.php";';
-echo '</script>'; 
-  	
-  }
+    $sql = "INSERT INTO schedule (game_name, groupA_team_name, groupB_team_name, place, s_date, s_time) VALUES ('$gname', '$teama', '$teamb', '$place', '$mdate', '$mtime')";
+    $data = mysqli_query($con, $sql);
 
+    if ($data) {
+        echo '<script type="text/javascript">'; 
+        echo 'alert("Schedule added successfully");'; 
+        echo 'window.location.href = "../add_scheduel.php";';
+        echo '</script>'; 
+    } else {
+        echo '<script type="text/javascript">'; 
+        echo 'alert("Error: ' . mysqli_error($con) . '");'; 
+        echo 'window.location.href = "../add_scheduel.php";';
+        echo '</script>'; 
+    }
 }
-
-
-
 ?>
