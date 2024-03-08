2cf82c780d839b2dc5d7f8ab401baca687989a66
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-03-08 16:06:50 +0530

    updated

--- reg_turnament.php
+++ reg_turnament.php
@@ -59,7 +59,7 @@
 				</div>
 			</div>
 		</div>
-	<?php
+		<?php
          include("commonpages/loginheader.php")
 		?>
 	</nav>
@@ -89,7 +89,7 @@
 						</div>
 							<div class="col-md-12">
 								<tr>
-								<h1>TOURNAMENT REGISTRATION!</h1>
+								<h1>TURNAMENT REGISTRATION!</h1>
 								</tr>
 								<tr>
 								<td>Name:</td>
@@ -165,29 +165,30 @@
 		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
 	</div>
 	
-
+	<!-- jQuery -->
 	<script src="js/jquery.min.js"></script>
-	
+	<!-- jQuery Easing -->
 	<script src="js/jquery.easing.1.3.js"></script>
-	
+	<!-- Bootstrap -->
 	<script src="js/bootstrap.min.js"></script>
-	
+	<!-- Waypoints -->
 	<script src="js/jquery.waypoints.min.js"></script>
-
+	<!-- Stellar Parallax -->
 	<script src="js/jquery.stellar.min.js"></script>
-
+	<!-- Carousel -->
 	<script src="js/owl.carousel.min.js"></script>
-
+	<!-- countTo -->
 	<script src="js/jquery.countTo.js"></script>
-	
+	<!-- Magnific Popup -->
 	<script src="js/jquery.magnific-popup.min.js"></script>
 	<script src="js/magnific-popup-options.js"></script>
-	
+	<!-- Google Map -->
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
 	<script src="js/google_map.js"></script>
-	
+	<!-- Main -->
 	<script src="js/main.js"></script>
 
 	</body>
 </html>
+
 
