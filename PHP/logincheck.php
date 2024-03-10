9d7d9f78b7bb7a70d44c6ecb227080d784fc0a59
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-03-10 17:50:48 +0530

    update

--- PHP/logincheck.php
+++ PHP/logincheck.php
@@ -3,7 +3,7 @@
 
     if (!isset($_SESSION['id'])) {
         header('Location: home.php');
-		ehco ("Invalid Session");
+		echo ("Invalid Session");
         exit(); // Add exit() to stop further execution
     }
 ?>
