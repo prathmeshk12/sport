1badea68a2956bcfe306ddbe2c9788d3a17f0642
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-03-12 19:58:54 +0530

    update

--- PHP/player_insert.php
+++ PHP/player_insert.php
@@ -19,20 +19,19 @@ $database="sportclub";
         
 
    $q="INSERT INTO player_detail(user_id, player_name, p_designation, p_type, team_name, contact, p_image)VALUES('$id','$name','$pdesign','$ptype','$team','$contact','$img') ";
-   echo "<script>alert('Submitted Sucessfully');window.location='../index1.php'</script>";
-      exit;
    $data=mysqli_query($con,$q);
-    if($data){
-     move_uploaded_file($_FILES['image']['tmp_name'],"../images1/player_images/$img");
-
-header('Location:../player_registration.php');
-exit;
-    }
-  else{
-    echo "<script>alert('Submitted Sucessfully');window.location='../index1.php'</script>";    //echo"faild";
-  }
+     if($data){
+      move_uploaded_file($_FILES['image']['tmp_name'],"../images1/player_images/$img");
+         echo '<script type="text/javascript">'; 
+ echo 'alert("Registration Successfully");'; 
+ echo 'window.location.href = "../player_registration.php";';
+ echo '</script>';
+     }
+   else{
+     echo"faild";
    }
-
+    }
+ 
 
 
 
