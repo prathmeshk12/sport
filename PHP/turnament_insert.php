2483e3f14198d16a205afb32cabf81c621548cac
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-03-12 18:50:12 +0530

    update

--- PHP/turnament_insert.php
+++ PHP/turnament_insert.php
@@ -4,8 +4,29 @@
    if(isset($_POST['btn_t'])){
     
 $id=$_POST['session_id'];
-echo $id;
 
+$game=$_POST['game'];
+   $name=$_POST['name'];
+   $email=$_POST['email'];
+   $contact=$_POST['contact'];
+   $address=$_POST['address'];
+   $date=$_POST['date'];
+   $team=$_POST['team'];
+  
+   include('./connection_db.php');
+   $q="INSERT INTO entry_register_tournament(user_id, game_name, name, email, contact, address, apply_date, team_name)VALUES('$id','$game','$name','$email','$contact','$address','$date','$team') ";
+  $data=mysqli_query($con,$q);
+    if($data){
+     
+      //  echo '<script type="text/javascript">';
+        header('Location: ../team_details.php'); 
+#echo 'alert("Registration Successfully");'; 
+//echo 'window.location.href = "../team_details.php";';
+echo '</script>';
+    }
+  else{
+    echo"faild";
+  }
    }
 
 
