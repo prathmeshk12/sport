6ca1588d2aefb49112b8a02041a0fdc3e444f65a
prathmeshk12 <155044248+prathmeshk12@users.noreply.github.com>
2024-03-12 13:53:41 +0530

    update

--- index1.php
+++ index1.php
@@ -203,12 +203,6 @@
 // Start the session
 session_start();
 
-// Check if the session variable 'id' is set
-if(!isset($_SESSION['id'])) {
-    // Redirect to the login page if the session variable 'id' is not set
-    header("Location: login.php");
-    exit();
-}
 
 // Now you can access $_SESSION['id'] anywhere in this file
 // For example:
